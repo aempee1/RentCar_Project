c4bc538af4aca1d1193645567f7459d0325acd98
Yahya Waederamae <6410110433@psu.ac.th>
2024-04-01 00:46:54 +0700

    random_key

--- my-be/config/admin.js
+++ my-be/config/admin.js
@@ -1,13 +1,23 @@
+const crypto = require('crypto');
+
+const generateSalt = () => {
+  return crypto.randomBytes(16).toString('base64');
+};
+
+const generateSecret = () => {
+  return crypto.randomBytes(16).toString('base64');
+};
+
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('9777631d0db2a37b54ecf26c3b6fc2f7f4e9f71face6a91f8ce99e300aa3aa9260bc2eb33c74c6e28ccd5d91acc059ed033baec16e0745946bba1b969b5e48b9b8e0cd20fcf34bfa338820f00ba63d7e716aed30535d54290ef8f5f5c139bd47ca6fca7ef5220013f8fab80b50c7238a00c97985fcc7f78b1c2953946a93cac5'),
+    secret: generateSecret(),
   },
   apiToken: {
-    salt: env('9777631d0db2a37b54ecf26c3b6fc2f7f4e9f71face6a91f8ce99e300aa3aa9260bc2eb33c74c6e28ccd5d91acc059ed033baec16e0745946bba1b969b5e48b9b8e0cd20fcf34bfa338820f00ba63d7e716aed30535d54290ef8f5f5c139bd47ca6fca7ef5220013f8fab80b50c7238a00c97985fcc7f78b1c2953946a93cac5'),
+    salt: generateSalt(),
   },
   transfer: {
     token: {
-      salt: env('9777631d0db2a37b54ecf26c3b6fc2f7f4e9f71face6a91f8ce99e300aa3aa9260bc2eb33c74c6e28ccd5d91acc059ed033baec16e0745946bba1b969b5e48b9b8e0cd20fcf34bfa338820f00ba63d7e716aed30535d54290ef8f5f5c139bd47ca6fca7ef5220013f8fab80b50c7238a00c97985fcc7f78b1c2953946a93cac5'),
+      salt: env('TRANSFER_TOKEN_SALT'),
     },
   },
   flags: {
--- my-be/config/server.js
+++ my-be/config/server.js
@@ -1,8 +1,13 @@
+const crypto = require('crypto');
+
+const generateRandomKey = () => {
+  return crypto.randomBytes(32).toString('hex');
+}
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: [generateRandomKey(), generateRandomKey()],
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
